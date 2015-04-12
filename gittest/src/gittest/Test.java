cf694c3f5159c811d6c191f25b7153330a99aa3f
qllu <qllu@smilingmobile.com>
2015-04-12 17:23:32 +0800

    12

--- gittest/src/gittest/Test.java
+++ gittest/src/gittest/Test.java
@@ -3,7 +3,7 @@ package gittest;
 public class Test {
 	public static void main(String[] args) {
 	    System.out.println("asds12323====");
-		System.out.println("asds12323xxx");
+		System.out.println("asds12323xxx123");
 	}
 
 }
